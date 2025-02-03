c576fefa253567bc8bdcea201dbb0a5b8c8cf577
Alex Sánchez C <asmalexander@hotmail.com>
2025-02-02 18:44:21 -0600

    Remove unnecessary whitespace and React.StrictMode from the main rendering in index.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
         <Routes>
             <Route exact path='/' element={<Home/>}/>
             <Route exact path='/crud' element={<CreateModule/>}/>
-
         </Routes>
     </BrowserRouter>
   );
